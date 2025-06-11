2f4d0f77e031015089df52aa432f7c8740485e0c
Spevan <67030925+Spevan@users.noreply.github.com>
2025-06-11 00:57:32 -0400

    Tower & Ammunition Early

--- GMDFinalProject/Assets/Scripts/Towers/scr_towerUnit.cs
+++ GMDFinalProject/Assets/Scripts/Towers/scr_towerUnit.cs
@@ -6,6 +6,11 @@ public class scr_towerUnit : MonoBehaviour
     public scr_tower cardData;
     public SphereCollider range;
 
+    public int cooldown, power, health;
+    public float timer;
+
+    public GameObject[] pooledProj;
+
     private Collider target;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +23,8 @@ public class scr_towerUnit : MonoBehaviour
         range = this.AddComponent<SphereCollider>();
         range.radius = cardData.range;
         range.isTrigger = true;
+
+        cooldown = (int)cardData.maxCooldown;
     }
 
     // Update is called once per frame
@@ -28,7 +35,26 @@ public class scr_towerUnit : MonoBehaviour
 
     void Attack()
     {
+        if (timer <= cooldown)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            ObjectPooling();
+        }
+    }
 
+    void ObjectPooling()
+    {
+        for (int i = 0; i < pooledProj.Length; i++)
+        {
+            if (!pooledProj[i].activeSelf)
+            {
+                pooledProj[i].SetActive(true);
+                pooledProj[i].transform.position = transform.position;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
