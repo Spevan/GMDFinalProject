ee2955ad62de8ec9d08e1c2919d35f35bcd3af76
Spevan <67030925+Spevan@users.noreply.github.com>
2025-10-10 17:59:42 -0400

    All Towers Added

--- GMDFinalProject/Assets/Scripts/Card Objs/scr_vehicle.cs
+++ GMDFinalProject/Assets/Scripts/Card Objs/scr_vehicle.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "scr_vehicle", menuName = "Scriptable Sheets/Vehicle")]
 public class scr_vehicle : scr_tower
 {
-    public int speed;
+    public float speed;
     public scr_hero unitEquipped;
 }
