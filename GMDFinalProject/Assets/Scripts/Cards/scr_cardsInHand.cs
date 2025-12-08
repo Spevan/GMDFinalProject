dd6ecda1547932c0c5706f6b9af5bc54cbcafae7
Spevan <67030925+Spevan@users.noreply.github.com>
2025-12-07 21:13:24 -0500

    Cards In Hand Prefab Change

--- GMDFinalProject/Assets/Scripts/Cards/scr_cardsInHand.cs
+++ GMDFinalProject/Assets/Scripts/Cards/scr_cardsInHand.cs
@@ -3,6 +3,7 @@ using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class scr_cardsInHand : scr_cards, IDragHandler, IDropHandler
 {
@@ -12,6 +13,7 @@ public class scr_cardsInHand : scr_cards, IDragHandler, IDropHandler
     public void Start()
     {
         playerCam = player.GetComponent<Camera>();
+        ChangeTransparency(0.5f);
     }
 
     public void CardDrawn(scr_card thisCard, scr_player thisPlayer, GameObject thisGUI)
@@ -24,6 +26,8 @@ public class scr_cardsInHand : scr_cards, IDragHandler, IDropHandler
 
     public override void OnPointerEnter(PointerEventData eventData)
     {
+        ChangeTransparency(1f);
+
         if (this.transform.position.x < (GUI.GetComponentInParent<Camera>().scaledPixelWidth / 2))
         {
             Vector3 spawnPos = new Vector3(gameObject.transform.position.x + (gameObject.transform.right.x / 2), gameObject.transform.position.y, gameObject.transform.position.z);
@@ -40,8 +44,46 @@ public class scr_cardsInHand : scr_cards, IDragHandler, IDropHandler
         temp.GetComponent<scr_cardDetails>().cardData = cardData;
     }
 
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        ChangeTransparency(0.5f);
+        base.OnPointerExit(eventData);
+    }
+
+    public void ChangeTransparency(float alphaAmnt)
+    {
+        Color currentColor = gameObject.GetComponent<Image>().color;
+        currentColor.a = alphaAmnt;
+        gameObject.GetComponent<Image>().color = currentColor;
+
+        foreach (TextMeshProUGUI text in GetComponentsInChildren<TextMeshProUGUI>())
+        {
+            currentColor = text.color;
+            currentColor.a = alphaAmnt;
+            text.color = currentColor;
+        }
+
+        foreach(Image image in GetComponentsInChildren<Image>())
+        {
+            currentColor = image.color;
+            currentColor.a = alphaAmnt;
+            image.color = currentColor;
+        }
+    }
+
     void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
+        foreach(scr_cardsInHand card in player.GetComponentInChildren<scr_guiManager>().hand.GetComponentsInChildren<scr_cardsInHand>())
+        {
+            if(!card.gameObject.Equals(gameObject))
+            {
+                card.ChangeTransparency(0f);
+            }
+            else
+            {
+                card.ChangeTransparency(1f);
+            }
+        }
         Vector2 pos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(playerCam.GetComponentInChildren<Canvas>().transform as RectTransform,
             Input.mousePosition, playerCam.GetComponentInChildren<Canvas>().worldCamera, out pos);
@@ -50,6 +92,11 @@ public class scr_cardsInHand : scr_cards, IDragHandler, IDropHandler
 
     void IDropHandler.OnDrop(UnityEngine.EventSystems.PointerEventData eventData)
     {
+        foreach (scr_cardsInHand card in player.GetComponentInChildren<scr_guiManager>().hand.GetComponentsInChildren<scr_cardsInHand>())
+        {
+            card.ChangeTransparency(0.5f);
+        }
+
         Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
